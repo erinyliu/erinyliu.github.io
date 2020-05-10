706c9258687da816700728d9efbf0e394ac3936b
Erin Liu <erin.y.liu@gmail.com>
2020-05-10 01:27:51 -0700

    revamp

---
+++ js/scrolling-nav.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $("#contact-form").on("submit", function(){
+    $.ajax({
+        url: "https://formspree.io/mwkrpvzr",
+        method: "POST",
+        data: {
+                name: $('#contact-form').find('input[name="name"]').val(),
+                email: $('#contact-form').find('input[name="email"]').val(),
+                message: $('#contact-form').find($('#message')).val(),
+                _subject: "You received a message from erinyliu.com",
+            },
+        dataType: "json"
+    });
+    $("#contact-form").css("display",'none')
+    $("#submission-msg").css("display", 'block')
+    return false;
+
+  })
+})
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 56)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+})(jQuery); // End of use strict
